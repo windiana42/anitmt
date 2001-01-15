9ede08dd366f5ae0801493ff4d9e886c06bc9ffe
Windiana42 <windiana@users.sf.net>
2001-01-15 12:32:59 +0000

    new vector/matrix library

---
+++ anitmt/lib/val/vect.cpp
@@ -0,0 +1,238 @@
+/*
+ * vect.cpp
+ * 
+ * Implementation of vector functions (namespaces vect, vect::internal). 
+ * 
+ * This is a part of the aniTMT animation project. 
+ * 
+ * Copyright (c) 2000--2001 by Wolfgang Wieser
+ * Bugs, suggestions to wwieser@gmx.de. 
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ * 
+ * This file is provided AS IS with no warranties of any kind.  The author
+ * shall have no liability with respect to the infringement of copyrights,
+ * trade secrets or any patents by this file or any part thereof.  In no
+ * event will the author be liable for any lost revenue or profits or
+ * other special, indirect and consequential damages.
+ * 
+ * This program is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ * 
+ * See the GNU General Public License for details.
+ * If you have not received a copy of the GNU General Public License,
+ * write to the 
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * Revision History:
+ *   Dec 2000   started writing
+ *
+ */
+
+#include <iostream>
+
+#include <vector.hpp>
+#include <matrix.hpp>
+
+// Most is done inline for speed increase. 
+
+namespace vect
+{
+	namespace internal
+	{
+		// Suffix 1 for 1-dim array (vector). 
+		ostream& stream_write_array1(ostream& s,const double *x,int n)
+		{
+			s << "<";
+			if(n>0)
+			{
+				s << *x;
+				for(int i=1; i<n; i++)
+				{  s << "," << x[i] ;  }
+			}
+			s << ">";
+			return(s);
+		}
+		
+		// For 2d-array subscript. 
+		// Array array with C columns; returns array[c][r]. 
+		// I decided to use a macro so that it expands to an expression 
+		// usable as lvalue as well. 
+		#define SUB(array,C,c,r)  array[c*C+r]
+		
+		// Suffix 2 for 2-dim array (matrix). 
+		ostream& stream_write_array2(ostream& s,const double *x,int C,int R)
+		{
+			static int warned=0;
+			if(!warned)
+			{
+				s << "[please implement vect::internal::stream_write_array2()]\n";
+				++warned;
+			}
+			
+			for(int r=0; r<R; r++)
+			{
+				s << "| ";
+				for(int c=0; c<C; c++)
+				{
+					s << SUB(x,C,c,r) << " ";
+				}
+				s << "|\n";
+			}
+			
+			return(s);
+		}
+		
+		// Multiplication of matrix a (pointer am; size ac, ar) with 
+		// matrix b (pointer bm; size bc, br). 
+		// Result is stored in matrix r (pointer rm; size rc, rr). 
+		// NOTE: Matrix multiplication is only defined if ac==br and 
+		//       ar==rr and bc==rc. If this is not the case, an 
+		//       exception is thrown. 
+		static inline void 
+			_matrix_mul(double *rm,int rc,int rr,
+			            const double *am,int ac,int ar,
+			            const double *bm,int bc,int br)
+		{
+			if(ac!=br || ar!=rr || bc!=rc)
+			{  throw vect::EX_Matrix_Illegal_Mult();  }
+			
+			for(int r=0; r<rr; r++)
+			{
+				for(int c=0; c<rc; c++)
+				{
+					double s=0.0;
+					for(int i=0; i<ac; i++)
+					{  s += SUB(am,ac,i,r) * SUB(bm,bc,c,i);  }
+					SUB(rm,rc,c,r)=s;  // store
+				}
+			}
+		}
+		
+		void matrix_mul(double *rm,int rc,int rr,
+		                const double *am,int ac,int ar,
+		                const double *bm,int bc,int br)
+				throw(vect::EX_Matrix_Illegal_Mult)
+		{
+			_matrix_mul(rm,rc,rr, am,ac,ar, bm,bc,br);
+		}
+		
+		// Version where r is multiplied with b and the result is stored in r. 
+		// Requires a temporary. 
+		void matrix_mul(double *rm,int rc,int rr,
+		                const double *bm,int bc,int br)
+				throw(vect::EX_Matrix_Illegal_Mult)
+		{
+			double am[rc*rr];
+			for(int i=rc*rr-1; i>=0; i--)  // copy rm to am 
+			{  am[i]=rm[i];  }
+			_matrix_mul(rm,rc,rr, am,rc,rr, bm,bc,br);
+		}
+		
+		// Invert a matrix. 
+		// Matrix m (pointer m; size c, r) is inverted; the result is 
+		// stored in matrix r (pointer rm; size rc, rr).
+		// NOTE: The result matrix r MUST BE SET TO THE IDENTITY MATRIX 
+		//       BEFORE CALLING THIS FUNCTION. 
+		// NOTE: Matrix inversion can only be done with quadratic matrices. 
+		//       If c!=r or rc!=rr or c!=rc, an exception is thrown. 
+		inline void _matrix_invert(double *rm,int rc,int rr,
+		                   double *m, int c, int r)
+				throw(vect::EX_Matrix_Illegal_Invert)
+		{
+			// Implementing the Gauss-Jordan algorithm. 
+			if(c!=r || rc!=rr || c!=rc)
+			{  throw vect::EX_Matrix_Illegal_Invert();  }
+			
+			// m is the identiy matrix. 
+			
+			int n=rc;  // = rr = c = r
+			for(int k=0; k<n; k++)
+			{
+				double tmp=SUB(m,n,k,k);
+				for(int j=0; j<n; j++)
+				{
+					SUB(m, n,j,k)/=tmp;
+					SUB(rm,n,j,k)/=tmp;
+				}
+				for(int i=0; i<n; i++)  if(i!=k)
+				{
+					double tmp=SUB(m,n,k,i);
+					for(int j=0; j<n; j++)
+					{
+						SUB(m, n,j,i) -= tmp * SUB(m, n,j,k);
+						SUB(rm,n,j,i) -= tmp * SUB(rm,n,j,k);
+					}
+				}
+			}
+		}
+		
+		// NOTE: m IS MODIFIED!! 
+		void matrix_invert(double *rm,int rc,int rr,
+		                   double *m, int c, int r)
+				throw(vect::EX_Matrix_Illegal_Invert)
+		{
+			_matrix_invert(rm,rc,rr,m,c,r);  // inline
+		}
+		
+		// m is copied; not modified. 
+		void matrix_invert_copy(double *rm,int rc,int rr,
+		                  const double *m, int c, int r)
+				throw(vect::EX_Matrix_Illegal_Invert)
+		{
+			
+			double tmp[c*r];
+			for(int i=c*r-1; i>=0; i--)  // copy m to tmp
+			{  tmp[i]=m[i];  }
+			_matrix_invert(rm,rc,rr,tmp,c,r);  // inline 
+		}
+		
+		// m is argument and return value. 
+		void matrix_invert_copy(double *m,int c,int r)
+				throw(vect::EX_Matrix_Illegal_Invert)
+		{
+			
+			double tmp[c*r];
+			// copy m to tmp and set m to the identity matrix. 
+			double *src=m,*dest=tmp;
+			for(int ic=0; ic<c; ic++)
+			{
+				for(int ir=0; ir<r; ir++)
+				{
+					*(dest++)=*(src++);
+					SUB(m,c,ic,ir) = (ic==ir) ? 1.0 : 0.0;
+				}
+			}
+			_matrix_invert(m,c,r,tmp,c,r);  // inline 
+		}
+		
+		
+		// Multiplication of matrix m (size mc,mr) with vector v (vn 
+		// dimensions); result is stored in rv (rn elements). 
+		// NOTE: If mc!=vn or mr!=rn, EX_Matrix_Illegal_VectMult is thrown. 
+		void matrix_mul_vect(
+			double *rv,int rn,
+			const double *m,int mc,int mr,
+			const double *v,int vn)  throw(vect::EX_Matrix_Illegal_VectMult)
+		{
+			if(mc!=vn || mr!=rn)
+			{  throw vect::EX_Matrix_Illegal_VectMult();  }
+			
+			for(int r=0; r<mr; r++)
+			{
+				double s=0.0;
+				for(int c=0; c<mc; c++)
+				{  s += SUB(m,mc,c,r) * v[c];  }
+				rv[r]=s;
+			}
+		}
+		
+	}
+}
+
---
+++ anitmt/test/test_val.cpp
@@ -0,0 +1,259 @@
+/*
+ * test_val.cpp
+ *
+ * Routines to test value library. 
+ *
+ * Copyright (c) 2000 by Wolfgang Wieser. 
+ *
+ * This is a top-secret file..\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b
+ * Use this file for anything you want.
+ *
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <val.hpp>
+
+using namespace values;
+
+int main()
+{
+	double da,db=1.1;
+	Scalar sa,sb(1),sc(sb),sd(db);
+	
+	da=sb;
+	if(1.00000000001==sb)
+		{  cerr << "equal\n";  }
+	if(!(sb!=1.00000000001))
+		{  cerr << "equal2\n";  }
+	sa=sb*sc;
+	sb=sa-0.00000000001;
+	if(sa==sb)
+		{  cerr << "equal3" "\n";  }
+	
+	sd-=1.10000000001;
+	if(!sd)
+		{  cerr << "zero\n";  }
+	if(double(sd)==0.0)
+		{  cerr << "OOPS!!\n";  }
+	
+	sa=2.0;
+	sb=2.0*sa;
+	sc=(sa*=sb)*2.0;
+	cerr << "res (16) =" << sc << "\n";
+	
+	sc=-sa;  cerr << "-" << sa << "=" << sc << "\n";
+	sc=+sa;  cerr << "+" << sa << "=" << sc << "\n";
+	
+	cerr << "sizes=" << sizeof(vect::vector<3>) << "," << sizeof(Vector) << "\n";
+	
+	Vector va(1.0,6.0,3.0),vb(2.0,4.0,2.0),vc,vd(vb);
+	cerr << vb << " -> abs, abs2: " << abs(vb) << ", " << abs2(vb) << "\n";
+	
+	cerr << va << "+" << vb << "=" << (va+vb) << "\n";
+	cerr << va << "*" << sa << "=" << (va*sa) << "\n";
+	cerr << vb << "/" << sa << "=" << (vb/sa) << "\n";
+	
+	vc=Vector(1.0,6.0,3.0000000001);
+	cerr << va << " == " << vc << " -> " << (va==vc) << "\n";
+	va=-vc;
+	
+	Vector v0;
+	cerr << "IsNull(" << v0 << ") = " << !v0 << "\n";
+	cerr << "IsNull(" << (v0+Vector(0,0,1)) << ") = " << !(v0+Vector(0,0,1)) << "\n";
+	
+	va=Vector(1.0,0.0,0.0);
+	vb=va;
+	cerr << "rotate(45):" << va << " -> " << rotateZ(rotateY(va,M_PI/4.0),M_PI/2.0) << "\n";
+	vb.rotateY(M_PI/4.0).rotateZ(M_PI/2.0);
+	cerr << "rotate(45):" << va << " -> " << vb << "\n";
+	
+	cerr << "Testing to_spherical/to_rectangular...";
+	srandom(getpid());
+	// Testing to_spherical, to_rectangular: 
+	for(int i=0; i<100; i++)
+	{
+		Vector vva(rand()/1000.0,rand()/1000.0,rand()/1000.0);
+		Vector sph1=to_spherical(vva);
+		Vector rec1=to_rectangular(sph1);
+		Vector sph2=vva;  sph2.to_spherical();
+		Vector rec2=sph2; rec2.to_rectangular();
+		if(sph1!=sph2)
+		{  cerr << "\nto_spherical: different results: " << 
+			sph1 << " " << sph2 << "\n";  }
+		if(rec2!=rec1)
+		{  cerr << "\nto_rectangular: different results. " << 
+			rec2 << " " << rec1 << "\n";  }
+		if(vva!=rec1)
+		{  cerr << "\nto_spherical/to_rectangular non-inverse: " << 
+			vva << " -> " << sph1 << " -> " << rec1 << "\n";  }
+	}
+	cerr << "done\n";
+	
+	// Speed test:
+	//cerr << "Speed test..."; 
+	//for(int i=0; i<100000; i++)
+	//{
+	//	va=vb+vc;
+	//	va/=10.0;
+	//	vc-=vb;
+	//}
+	//cerr << "passed.\n";
+	
+	va=Vector(2,4,5);
+	vb=va;
+	cerr << "normalize(" << va << ") = " << 
+		normalize(va) << " = " << vb.normalize() << "\n";
+	if(normalize(va) != vb || 
+	   abs(vb) != 1.0)
+	{  cerr << "*** ERROR!! normalize-abs=" << vb.abs() << "\n";  }
+	
+	cerr << "Testing abs...";
+	if(va.abs() != abs(va))
+	{  cerr << "abs() ERROR: " << va.abs() << " != " 
+		<< abs(va) << "\n";  }
+	else
+	{  cerr << "done\n";  }
+	
+	va=Vector(-2,3.5,-7);
+	vb=va;
+	cerr << "Vector: " << va << "\n";
+	cerr << "Mirrors: " << 
+		mirrorX(va) << 
+		mirrorY(va) << 
+		mirrorZ(va) << 
+		mirror(va) << "\n";
+	cerr << "Mirrors: " << 
+		va.mirrorX() << 
+		va.mirrorX().mirrorY() << 
+		va.mirrorY().mirrorZ() << 
+		va.mirrorZ().mirror() << "\n";
+	
+	va=vb;
+	cerr << "Trans: " << 
+		transX(va,-6) << 
+		transY(va,-6) << 
+		transZ(va,-6) << "\n";
+	cerr << "Trans: " << 
+		va.transX(-6) << 
+		va.transX(6).transY(-6) << 
+		va.transY(6).transZ(-6) << "\n";
+	
+	va=vb;
+	cerr << "Scale: " << 
+		scaleX(va,-2) << 
+		scaleY(va,-2) << 
+		scaleZ(va,-2) << "\n";
+	cerr << "Scale: " << 
+		va.scaleX(-2) << 
+		va.scaleX(-0.5).scaleY(-2) << 
+		va.scaleY(-0.5).scaleZ(-2) << "\n";
+	
+	cerr << "Testing multiplication...";
+	for(int i=0; i<100; i++)
+	{
+		va=Vector(rand()/1000,rand()/1000,rand()/1000);
+		vb=Vector(rand()/1000,rand()/1000,rand()/1000);
+		vc=cross(va,vb);
+		sa=va*vc;
+		sb=dot(vb,vc);
+		sc=abs(va)*abs(vb)*(sin(angle(va,vb)));
+		if(sa!=0 || sb!=0)
+		{  cerr << "*** ERROR (scalar/vector mul) (" << 
+			sa << ", " << sb << ")\n";  }
+		else if(abs(vc)-sc > 0.01)
+		{  cerr << "*** ERROR (scalar/vector mul) " << 
+			abs(vc) << " != " << sc << "; delta=" << 
+			abs(vc)-sc << "\n";  }
+	}
+	cerr << "done\n";
+	
+	Matrix ma,mb(Matrix::ident),mc(Matrix::null),md;
+	
+	ma(2,0,5.0)(2,2,7.0);
+	cerr << ma;
+	cerr << "idx:" << ma[0][0] << "," << ma[1][0] << "," 
+	               << ma[2][0] << "," << ma[2][2] << "\n";
+	
+	cerr << "ident? (should be 1,0) " << !mb << "," << !ma << "\n";
+	cerr << "null? (should be 1,0) " << mc.is_null() << "," << mb.is_null() << "\n";
+	
+	//cerr << ma*10 << ma/2 << 2*ma ;
+	//ma*=10;  cerr << ma;  ma/=2;  cerr << ma;
+	
+	cerr << "Simple Matrix*Vector test...";
+	va=Vector(3.77,8.91,-13.1);
+	vb=va;
+	mc=mb*2;
+	if(mb*va != vb || mc*va != vb*2.0 || va*mc != vb*2.0)
+	{  cerr << "\n*** ERROR in Matrix*Vector (1).\n";  }
+	else
+	{
+		va*=mc;
+		vb*=2;
+		if(va != vb)
+		{  cerr << "\n*** ERROR in Matrix*Vector (2).\n";  }
+		else
+		{  cerr << "done\n";  }
+	}
+	
+	cerr << "Matrix*Matrix / invert test...";
+	for(int i=0; i<100; i++)
+	{
+		for(int c=0; c<3; c++)
+			for(int r=0; r<3; r++)
+				mc(c,r,rand()/1000.0);
+		
+		if(mc*mb != mc)
+		{  cerr << "\n*** ERROR in Matrix*Matrix (1).\n";  }
+		if(mb*mc != mc)
+		{  cerr << "\n*** ERROR in Matrix*Matrix (2).\n";  }
+		ma=mc;
+		mc*=mb;
+		if(mc != ma)
+		{  cerr << "\n*** ERROR in Matrix*Matrix (3).\n";  }
+		
+		// mc = random;  ma = mc;  mb = identity matrix
+		
+		ma.invert();
+		if(ma*mc!=mb)
+		{  cerr << "\n*** ERROR in invert() (1).\n";  }
+		
+		ma=invert(mc);
+		if(ma*mc!=mb)
+		{  cerr << "\n*** ERROR in invert() (2).\n";  }
+	}
+	cerr << "done\n";
+	
+	cerr << "Matrix+-Matrix / -Matrix test...";
+	for(int i=0; i<100; i++)
+	{
+		Matrix mrp,mrm,mrn;
+		for(int c=0; c<3; c++)
+			for(int r=0; r<3; r++)
+			{
+				double ra=rand()/1000.0,rb=rand()/1000.0;
+				ma(c,r,ra);
+				mb(c,r,rb);
+				mrp(c,r,ra+rb);
+				mrm(c,r,ra-rb);
+				mrn(c,r,-ra);
+			}
+		md=ma;
+		mc=ma+mb;
+		md+=mb;
+		if(mc!=md || mc!=mrp || mc*0.5!=(ma/2.0)+(mb*0.5))
+		{  cerr << "\n*** ERROR in matrix+matrix\n";  }
+		md=ma;
+		mc=ma-mb;
+		md-=mb;
+		if(mc!=md || mc!=mrm || mc*0.5!=(ma/2.0)-(mb*0.5))
+		{  cerr << "\n*** ERROR in matrix-matrix\n";  }
+		if(-ma!=mrn || (-ma)*2.0 != (ma*(-2.0)))
+		{  cerr << "\n*** ERROR in -matrix\n";  }
+	}
+	cerr << "done\n";
+	
+	return(0);
+}
