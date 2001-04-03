cdf42aadf48ede0502174f772329594cf9bfc00f
onno <>
2001-04-03 14:29:33 +0000

    Option for forcing regeneration of scripts added

--- anitmt/autogen.sh
+++ anitmt/autogen.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Automatically calls automake and autoconf the first time if necessary...
 # License: LGPL. USE AT YOUR OWN RISK!!
+# Copyright: Onno Kortmann
 # Needs the unix tools bash, wc and which.
+#
+# Command line arguments:
+# (none) - create files as necessary
+# (-f) - recreate all files (a bit like autoreconf, but not really :) 
+
+
 
 # check for aclocal, autoconf and automake
 NEEDED="aclocal autoconf automake"
@@ -15,23 +22,26 @@ if test `echo $NEEDED|wc -w` != `which $NEEDED|wc -l`; then
     echo "Come back later.";
     exit 1;
 else
-    printf "\nOk. All neccessary tools are available.\n";
+    printf "\nOk. All neccessary tools are available.\n\n";
 fi
 
+# FIXME: Correct argument handling... there's only "-f" now, so that's
+# no problem yet
+
 # Make aclocal.m4
-if test ! -f aclocal.m4; then
+if test -n "$1" -o ! -f aclocal.m4; then
     echo "Creating aclocal.m4.";
     aclocal || exit 2;
 fi
 
 # Make configure
-if test ! -f configure; then
+if test -n "$1" -o ! -f configure; then
     echo "Creating configure.";
     autoconf || exit 2;
 fi
 
 # Make Makefile.in(s)
-if test ! -f Makefile.in; then
+if test -n "$1" -o ! -f Makefile.in; then
     echo "Creating Makefile.ins.";
     automake || exit 2;
 fi
