3faef6ee9e5b10be6466a6f9b8c085268699aa61
triplespark <github@triplespark.net>
2002-01-18 17:19:27 +0000

    Deleting adl/

--- anitmt/anitmt-calc/input/adl/flexlexer_global.h
+++
@@ -1,4 +0,0 @@
-// file needed for self coded version of lib/flex/FlexLexer.h
-//************************************************************
-// you can add global declarations for your  flexlexer here:
-// ex: #include <special.h>
--- anitmt/anitmt-calc/input/adl/flexlexer_public.h
+++
@@ -1,4 +0,0 @@
-// file needed for self coded version of lib/flex/FlexLexer.h
-//************************************************************
-// you can add public members to your flexlexer object here:
-// ex: YYSTYPE *yylval;
