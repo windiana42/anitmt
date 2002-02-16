8934946b7f277a6d324cb1487f142194d8286d11
triplespark <github@triplespark.net>
2002-02-16 23:33:20 +0000

    *** empty log message ***

---
+++ anitmt/hlib/misc/msecelapsedr.c
@@ -0,0 +1,37 @@
+/*
+ * msecelapsedr.c 
+ * 
+ * Copyright (c) 2002 by Wolfgang Wieser (wwieser@gmx.de) 
+ * 
+ * This file may be distributed and/or modified under the terms of the 
+ * GNU General Public License version 2 as published by the Free Software 
+ * Foundation. 
+ * 
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
+
+#include <hlib/prototypes.h>
+
+#include <unistd.h>
+#include <sys/time.h>
+
+/* Calc the time between *old and *current in msec. 
+ * Current may be NULL (then it is queried from the system). 
+ * Result rounded at division (see also msec_elapsed()). 
+ */
+long msec_elapsed_r(const struct timeval *old,const struct timeval *current)
+{
+	struct timeval _curr;
+	long du;
+	if(!current)
+	{
+		gettimeofday(&_curr,NULL);
+		current=&_curr;
+	}
+	du = (current->tv_usec - old->tv_usec);
+	return(
+		(current->tv_sec  - old->tv_sec)*1000L + 
+		((du<0L) ? (du-500) : (du+500) )/1000L );
+}
