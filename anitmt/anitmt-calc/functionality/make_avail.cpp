2bd0834880b601e254fa2f0e78068cbc4e8f8723
Windiana42 <windiana@users.sf.net>
2002-02-16 20:31:41 +0000

    object and move_straight started

---
+++ anitmt/anitmt-calc/functionality/make_avail.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************************/
+/**   This file makes the generated functionality availible		    **/
+/*****************************************************************************/
+/**									    **/
+/** Author: Martin Trautmann						    **/
+/**									    **/
+/** EMail:   martintrautmann@gmx.de					    **/
+/**									    **/
+/** License: GPL - free and without any warranty - read COPYING             **/
+/**									    **/
+/** Package: AniTMT							    **/
+/**									    **/
+/*****************************************************************************/
+
+#include "base_func.hpp"
+#include "functionality.hpp"
+#include "scalar.hpp"
+#include "object.hpp"
+
+namespace functionality 
+{
+  void make_nodes_availible()
+  {
+    make_base_func_nodes_availible();
+    make_functionality_nodes_availible();
+    make_scalar_nodes_availible();
+    make_object_nodes_availible();
+  }
+}
---
+++ anitmt/anitmt-calc/functionality/make_avail.hpp
@@ -0,0 +1,18 @@
+/*****************************************************************************/
+/**   This file makes the generated functionality availible		    **/
+/*****************************************************************************/
+/**									    **/
+/** Author: Martin Trautmann						    **/
+/**									    **/
+/** EMail:   martintrautmann@gmx.de					    **/
+/**									    **/
+/** License: GPL - free and without any warranty - read COPYING             **/
+/**									    **/
+/** Package: AniTMT							    **/
+/**									    **/
+/*****************************************************************************/
+
+namespace functionality 
+{
+  void make_nodes_availible();
+}
