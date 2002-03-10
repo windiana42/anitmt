2863e92c1b1f796e054260a0a72cc7d57ae36eaf
Windiana42 <windiana@users.sf.net>
2002-03-10 14:15:29 +0000

    tell funcgen which basic operators are availible

---
+++ anitmt/lib/solve/base_operators.cpp
@@ -0,0 +1,96 @@
+/*****************************************************************************/
+/**   This file offers functions that tell availible basic operators	    **/
+/*****************************************************************************/
+/**									    **/
+/** Author: Martin Trautmann						    **/
+/**									    **/
+/** EMail:   martintrautmann@gmx.de					    **/
+/**									    **/
+/** License: LGPL - free and without any warranty - read COPYING            **/
+/**									    **/
+/** Package: AniTMT							    **/
+/**									    **/
+/*****************************************************************************/
+
+#include "base_operators.hpp"
+
+namespace solve
+{
+
+  bool Basic_Operator::is_avail( std::string function ) const
+  {
+    return functions.find( function ) != functions.end();
+  }
+
+  //! get parameter types of function (first is return type)
+  const std::list<type> &Basic_Operator::get_types
+  ( std::string function ) 
+  {
+    return functions[function];
+  }
+  const std::list<std::string> &Basic_Operator::get_required_functions()
+    const
+  {
+    return required_functions;
+  }
+  std::string Basic_Operator::get_real_name()
+    const
+  {
+    return real_name;
+  }
+
+  void Basic_Operator::add_1_arg_fun( type return_type, std::string name, 
+				 type arg1 )
+  {
+    std::list<type> &args = functions[name];
+    args.push_back( return_type );
+    args.push_back( arg1 );
+  }
+  void Basic_Operator::add_2_arg_fun( type return_type, std::string name, 
+				 type arg1, type arg2 )
+  {
+    std::list<type> &args = functions[name];
+    args.push_back( return_type );
+    args.push_back( arg1 );
+    args.push_back( arg2 );
+  }
+  void Basic_Operator::add_3_arg_fun( type return_type, std::string name, 
+				 type arg1, type arg2, type arg3 )
+  {
+    std::list<type> &args = functions[name];
+    args.push_back( return_type );
+    args.push_back( arg1 );
+    args.push_back( arg2 );
+    args.push_back( arg3 );
+  }
+  void Basic_Operator::add_4_arg_fun( type return_type, std::string name, 
+				 type arg1, type arg2, type arg3, type arg4 )
+  {
+    std::list<type> &args = functions[name];
+    args.push_back( return_type );
+    args.push_back( arg1 );
+    args.push_back( arg2 );
+    args.push_back( arg3 );
+    args.push_back( arg4 );
+  }
+
+  bool Available_Basic_Operators::is_operator( std::string operator_name ) 
+    const
+  {
+    return basic_operators.find(operator_name) != basic_operators.end();
+  }
+  const Basic_Operator &Available_Basic_Operators::get_operator
+  ( std::string basic_operator)
+  {
+    return basic_operators[basic_operator];
+  }
+  Available_Basic_Operators::Available_Basic_Operators()
+  {
+    Basic_Operator &cur_operator = basic_operators["1_operand_operator"];
+    cur_operator.real_name = "Basic_Operator_for_1_Operand";
+    cur_operator.add_2_arg_fun( none, "is_operand_ok", operand, info );
+    cur_operator.add_1_arg_fun( boolean, "is_operand_enough", operand );
+    cur_operator.add_1_arg_fun( result, "calc_result", operand );
+    cur_operator.required_functions.push_back("calc_result");
+  }
+}
---
+++ anitmt/lib/solve/base_operators.hpp
@@ -0,0 +1,61 @@
+/*****************************************************************************/
+/**   This file offers functions that tell availible basic operators	    **/
+/*****************************************************************************/
+/**									    **/
+/** Author: Martin Trautmann						    **/
+/**									    **/
+/** EMail:   martintrautmann@gmx.de					    **/
+/**									    **/
+/** License: LGPL - free and without any warranty - read COPYING            **/
+/**									    **/
+/** Package: AniTMT							    **/
+/**									    **/
+/*****************************************************************************/
+
+#ifndef __Solve_Basic_Operator__
+#define __Solve_Basic_Operator__
+
+#include <map>
+#include <set>
+#include <list>
+#include <string>
+
+namespace solve
+{
+  enum type{ none, boolean, result, operand, info };
+  class Basic_Operator
+  {
+  public:
+    bool is_avail( std::string function ) const;
+    //! get parameter types of function (first is return type)
+    const std::list<type> &get_types( std::string function );
+    //! get functions that are required
+    const std::list<std::string> &get_required_functions() const;
+    //! get real class name of the operator
+    std::string get_real_name() const;
+  private:
+    friend class Availible_Basic_Operators;
+    std::map< std::string, std::list<type> > functions;
+    std::list< std::string > required_functions;
+    std::string real_name;
+    void add_1_arg_fun( type return_type, std::string name, type arg1 );
+    void add_2_arg_fun( type return_type, std::string name, type arg1, 
+			type arg2 );
+    void add_3_arg_fun( type return_type, std::string name, type arg1, 
+			type arg2, type arg3 );
+    void add_4_arg_fun( type return_type, std::string name, type arg1, 
+			type arg2, type arg3, type arg4 );
+  };
+  class Available_Basic_Operators
+  {
+  public:
+    Available_Basic_Operators();
+
+    bool is_operator( std::string basic_operator ) const;
+    const Basic_Operator &get_operator( std::string basic_operator );
+  private:
+    std::map<std::string,Basic_Operator> basic_operators;
+  };
+}
+
+#endif
