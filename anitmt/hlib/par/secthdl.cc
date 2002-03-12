b86552326584e919426b66f2bd71089cb0997898
triplespark <github@triplespark.net>
2002-03-12 20:10:54 +0000

    *** empty log message ***

---
+++ anitmt/hlib/par/secthdl.cc
@@ -0,0 +1,30 @@
+/*
+ * secthdl.cc
+ * 
+ * Implementation of section parameter handler. 
+ * 
+ * Copyright (c) 2002 by Wolfgang Wieser (wwieser@gmx.de) 
+ * 
+ * This file may be distributed and/or modified under the terms of the 
+ * GNU Lesser General Public License version 2.1 as published by the 
+ * Free Software Foundation. (See COPYING.LGPL for details.)
+ * 
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
+
+#include "secthdl.h"
+
+
+namespace par
+{
+
+int SectionParameterHandler::Attach(const char *section,Section *top=NULL)
+{
+	Section *s=parmanager()->FindSection(section,top);
+	if(!s)  return(-1);
+	return(Attach(s));
+}
+
+}  // end of namespace par 
