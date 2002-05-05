7fdf380b10db2162c6a84acd84393e0017d2e351
triplespark <github@triplespark.net>
2002-05-05 14:36:47 +0000

    Neutral type.

---
+++ anitmt/lib/val/neutral.cpp
@@ -0,0 +1,43 @@
+/*
+ * neutral.cpp
+ * 
+ * Implementation of non-inline support stuff for neutral type. 
+ * 
+ * Copyright (c) 2002 by Wolfgang Wieser (wwieser@gmx.de) 
+ * 
+ * This file may be distributed and/or modified under the terms of the 
+ * GNU General Public License version 2 as published by the Free Software 
+ * Foundation. 
+ * 
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
+
+#include "neutral.hpp"
+
+namespace vect
+{
+
+std::ostream& operator<<(std::ostream& s,const Neutral0 &)
+{
+	s << "Neutral0";
+	return(s);
+}
+
+std::ostream& operator<<(std::ostream& s,const Neutral1 &)
+{
+	s << "Neutral1";
+	return(s);
+}
+
+
+#if 0
+std::ostream& operator<<(std::ostream& s,const Neutral &n)
+{
+	s << (n.n ? "Neutral1" : "Neutral0");
+	return(s);
+}
+#endif
+
+}  // namespace end
---
+++ anitmt/lib/val/neutral.hpp
@@ -0,0 +1,75 @@
+/*
+ * neutral.hpp
+ * 
+ * Header for the neutral value type. 
+ * 
+ * Copyright (c) 2002 by Wolfgang Wieser (wwieser@gmx.de) 
+ * 
+ * This file may be distributed and/or modified under the terms of the 
+ * GNU General Public License version 2 as published by the Free Software 
+ * Foundation. 
+ * 
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
+
+#ifndef _NS_vect_neutral_HPP_
+#define _NS_vect_neutral_HPP_ 1
+
+#include <iostream>
+
+namespace vect
+{
+
+class Neutral0
+{
+	public:
+		Neutral0(const Neutral0 &) { }
+		Neutral0() { }
+		~Neutral0() { }
+		
+		Neutral0 &operator=(const Neutral0 &)  {  return(*this);  }
+		
+		friend std::ostream& operator<<(std::ostream& s,const Neutral0 &n);
+};
+
+class Neutral1
+{
+	public:
+		Neutral1(const Neutral0 &) { }
+		Neutral1() { }
+		~Neutral1() { }
+		
+		Neutral1 &operator=(const Neutral1 &)  {  return(*this);  }
+		
+		friend std::ostream& operator<<(std::ostream& s,const Neutral1 &n);
+};
+
+
+#if 0
+class Neutral
+{
+	private:
+		short int n;
+	public:
+		Neutral(const Neutral &s) : n(s.n) { }
+		// Set up neutral elem. s must be 0 or 1. 
+		// 0 -> addition neutral
+		// 1 -> multiplication neutral
+		Neutral(int s) : n(s) { }
+		~Neutral() { }
+		
+		// Mostly used internally by matrix/vector code: 
+		operator bool()  {  return(n);  }
+		
+		operator int() const  {  return(n);  }
+		int val() const  {  return(n);  }
+		
+		friend std::ostream& operator<<(std::ostream& s,const Neutral &n);
+};
+#endif
+
+}  // namespace end 
+
+#endif  /* _NS_vect_neutral_HPP_*/
