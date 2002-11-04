5b435faa32ceb5d2cfae4e7c3e23297021c4275e
Windiana42 <windiana@users.sf.net>
2002-11-04 00:47:58 +0000

    to set breakpoints in the solve system

---
+++ anitmt/anitmt-calc/functionality/break_point.hpp
@@ -0,0 +1,16 @@
+/*****************************************************************************/
+/** AniTMT -- A program for creating foto-realistic animations		    **/
+/**   This file belongs to the component anitmt-calc			    **/
+/*****************************************************************************/
+/**									    **/
+/** Author: Martin Trautmann						    **/
+/**									    **/
+/** Website: http://www.anitmt.org/      				    **/
+/** EMail:   anitmt@theofel.de						    **/
+/**									    **/
+/** License: GPL - free and without any warranty - read COPYING             **/
+/**									    **/
+/*****************************************************************************/
+
+// function to set breakpoints
+void _break_point_function();
